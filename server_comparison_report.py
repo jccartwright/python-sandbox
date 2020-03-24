dc9bb96bfbf299f5193941c36d209877458a2a91
jcc <john.c.cartwright@comcast.net>
2020-03-23 18:29:34 -0600

    major re-write of server_comparison_report.py

--- server_comparison_report.py
+++ server_comparison_report.py
@@ -13,60 +13,68 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def main(args):
-    # augment each server name with port number. all servers must use same port.
-    # servers = [f"{i}:{args.port}" for i in args.target_server]
+    if args.diff and len(args.server) != 2:
+        logging.error("--diff can only be used with 2 servers")
+        sys.exit(1)
+
+    # initialize server list with command line values
     servers = []
-    for i in range(0,2):
-        name = args.server[i]
-        password = args.password[i]
-        username = args.username[i]
+    for i in zip(args.server, args.username, args.password):
+        name = i[0]
+        username = i[1]
+        password = i[2]
         uri = f"{name}:{args.port}"
         token = get_token(username, password, uri)
-        mapservice_list = []
-        get_service_names(uri, mapservice_list)
-        imageservice_list = []
-        get_service_names(uri, imageservice_list, None, 'ImageServer')
+
+        # get a list of all services
+        service_list = []
+        get_service_names(uri, service_list)
+        get_service_names(uri, service_list, None, 'ImageServer')
+
+        # dictionary of each service's relevant properties
+        capabilities = {}
+        for service in service_list:
+            capabilities[service] = get_capabilities(name, service, token)
 
         servers.append({'name': name, 'username': username, 'password': password, 'port': args.port, 'token': token,
-                        'mapservices': mapservice_list, 'imageservices': imageservice_list})
+                        'services': capabilities})
 
-    # build a list of unique service names across all servers
-    all_mapservices = []
-    for server in servers:
-        all_mapservices.extend(server['mapservices'])
+    # print all services
+    # server_name = servers[0]['name']
+    # capabilities = servers[0]['services']
+    # print("server\tservice\ttype\twms\twcs\tantialiasing")
+    # for service in capabilities:
+    #     print(f"{server_name}\t{service}\t{capabilities[service]['type']}\t{capabilities[service]['wms']}\t{capabilities[service]['wcs']}\t{capabilities[service]['antialiasing']}")
 
-    # convert to a list of unique service names
-    mapservices = sorted(list(set(all_mapservices)))
+    # services not on both servers
+    server0_services = set(servers[0]['services'].keys())
+    server1_services = set(servers[1]['services'].keys())
+    # expect to be sorted
+    if server0_services == server1_services:
+        print(f"{servers[0]['name']} and {servers[1]['name']} have the same services")
+    else:
+        diff0 = server0_services.difference(server1_services)
+        diff1 = server1_services.difference(server0_services)
+        print(diff0)
+        print(diff1)
 
-    all_imageservices = []
-    for server in servers:
-        all_imageservices.extend(server['imageservices'])
 
-    # convert to a list of unique service names
-    imageservices = sorted(list(set(all_imageservices)))
+def get_capabilities(server, service, token):
+    service_info = get_service_info(token, server, service)
+    wms = extension_enabled(service_info, 'WMSServer')
+    wcs = extension_enabled(service_info, 'WCSServer')
+    antialiasing = service_info['properties']['antialiasingMode']
+    return {'type': service_info['type'], 'wms': wms, 'wcs': wcs, 'antialiasing': antialiasing}
 
-    # report services not found on both servers
-    print('Map Services')
-    print(f"name\t{servers[0]['name']}\t{servers[1]['name']}")
-    for service in mapservices:
-        if service not in servers[0]['mapservices'] or service not in servers[1]['mapservices']:
-            print(f"{service}\t{service in servers[0]['mapservices']}\t{service in servers[1]['mapservices']}")
 
-    print('\n')
-    print('Image Services')
-    print(f"name\t{servers[0]['name']}\t{servers[1]['name']}")
-    for service in imageservices:
-        if service not in servers[0]['imageservices'] or service not in servers[1]['imageservices']:
-            print(f"{service}\t{service in servers[0]['imageservices']}\t{service in servers[1]['imageservices']}")
-
-    # for service in services:
-    #     try:
-    #         service_info = get_service_info(token, server, service)
-    #     except Exception as e:
-    #         logging.warning(f"unable to get service info for {service}")
-    #         continue
-    #     # print(json.dumps(service_info, indent=2))
-    #     print(f"{service}: antialiasing set to {service_info['properties']['antialiasingMode']}")
+def extension_enabled(service_info, extension='WMSServer'):
+    """find the specified extension section in the service_info and return whether it is enabled"""
+    for i in service_info['extensions']:
+        if i['typeName'] == extension:
+            if i['enabled'] == 'true':
+                return True
+            else:
+                return False
 
 
 def get_service_names(hostname, services=[], folder=None, type='MapServer'):
@@ -95,7 +103,6 @@ def get_token(username, password, servername):
     if r.status_code != 200:
         raise Exception("Error while fetching tokens from admin URL. Please check the URL and try again.")
     data = r.json()
-    print(data)
 
     if not assert_json_success(data):
         # logging.warning(data)
@@ -128,12 +135,13 @@ def assert_json_success(json):
     else:
         return True
 
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.WARN)
 
     # setup command line arguments
     arg_parser = argparse.ArgumentParser(
-        description="""report on the differences in map/image services on the specified server(s)"""
+        description="""report on the properties of map/image services on the specified server(s)"""
     )
     arg_parser.add_argument("--username", action="append", required=True, help="user name")
     arg_parser.add_argument("--password", action="append", required=True, help="password")
