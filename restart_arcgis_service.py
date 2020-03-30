a9db5a12e3b8914579c6cb4eb7eadb879397bc89
jcc <john.c.cartwright@comcast.net>
2020-03-30 14:39:24 -0600

    add last restart report

--- restart_arcgis_service.py
+++ restart_arcgis_service.py
@@ -6,6 +6,7 @@ import logging
 import sys
 from urllib.parse import urlencode
 import urllib3
+from datetime import datetime
 
 # ignore warning about NGDC-signed certificate
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -17,9 +18,33 @@ def main(args):
 
     token = get_token(args.username, args.password, server)
 
+    if args.report:
+        timestamp = get_lastmodified_date(token, server, args.service, args.service_type)
+        # remove the milliseconds
+        timestamp = int(timestamp / 1000)
+        print(f"{args.service} last started on {datetime.fromtimestamp(timestamp)}")
+        return
+
     stop_service(token, server, args.service, args.service_type)
     time.sleep(30)
     start_service(token, server, args.service, args.service_type)
+
+
+def get_lastmodified_date(token, servername, servicename, service_type):
+    url = f"https://{servername}/arcgis/admin/services/{servicename}.{service_type}/lifecycleinfos"
+    params = urlencode({'token': token, 'f': 'json'})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+
+    r = requests.post(url, headers=headers, data=params, verify=False)
+    if r.status_code != 200:
+        raise Exception("Error while getting lifecycleinfo from admin URL. Please check the URL and try again.")
+    data = r.json()
+
+    if not assert_json_success(data):
+        # logging.warning(data)
+        raise Exception("Error: response object represents an error.")
+
+    return data['lastmodified']
 
 
 def stop_service(token, servername, servicename, service_type):
@@ -29,7 +54,7 @@ def stop_service(token, servername, servicename, service_type):
 
     r = requests.post(url, headers=headers, data=params, verify=False)
     if r.status_code != 200:
-        raise Exception("Error while fetching tokens from admin URL. Please check the URL and try again.")
+        raise Exception("Error while stopping the service via the admin URL. Please check the URL and try again.")
     data = r.json()
 
     if not assert_json_success(data):
@@ -46,7 +71,7 @@ def start_service(token, servername, servicename, service_type):
 
     r = requests.post(url, headers=headers, data=params, verify=False)
     if r.status_code != 200:
-        raise Exception("Error while fetching tokens from admin URL. Please check the URL and try again.")
+        raise Exception("Error while starting the service via the admin URL. Please check the URL and try again.")
     data = r.json()
 
     if not assert_json_success(data):
@@ -94,6 +119,8 @@ if __name__ == "__main__":
     arg_parser.add_argument('--service', required=True, help="service name including folder")
     arg_parser.add_argument('--service_type', default='MapServer', choices=['MapServer', 'ImageServer'],
                             help="type of service, default is MapServer")
+    arg_parser.add_argument("-r", "--report", help="only report on last restart times", action="store_true")
+
     args = arg_parser.parse_args()
 
     main(args)
